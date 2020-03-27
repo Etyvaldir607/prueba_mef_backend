990a9b8356035379283324bbe26a1182b2c599a8
Rodrigo Garcia <strysg@riseup.net>
2020-03-27 11:31:20 -0400

    Corrigiendo respuesta de logs y quitando lineas comentadas

--- src/application/api/controllers/system/LogsController.js
+++ src/application/api/controllers/system/LogsController.js
@@ -14,18 +14,33 @@ module.exports = function setupLogsController (services) {
 
     try {
       let result = await Log.findAll(data);
-      // res.send({
-      //   count: result.data.count,
-      //   datos: {
-      //     logs: result.data.rows
-      //   }
-      // });
-      res.send({
-        datos: {
-          coung: result.data.count,
-          datos: result.data.rows
+      // preparando el tipo de respuesta
+      let respuesta;
+      if (Log.logsConfig) {
+        if (Log.logsConfig.storage == 'filesystem') {
+          respuesta = {
+            datos: {
+              count: result.data.count,
+              datos: result.data.rows
+            }
+          };
+        } else {
+          respuesta = {
+            count: result.data.count,
+            datos: {
+              logs: result.data.rows
+            }
+          };
         }
-      });
+      } else {
+        respuesta = {
+          count: result.count,
+          datos: {
+            logs: result.rows
+          }
+        };
+      }
+      res.send(respuesta);
     } catch (e) {
       return next(e);
     }
